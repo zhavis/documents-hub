00aba76036863478f08e41b4a5b85174cb110e4e
zhavihosseini <133713566+zhavis@users.noreply.github.com>
2025-12-08 02:59:09 +0330

    Update route.ts

--- app/api/tree/route.ts
+++ app/api/tree/route.ts
@@ -1,27 +1,26 @@
 import { NextResponse } from "next/server";
 import { fetchTree, TreeItem } from "@/lib/fetchDocs";
 
-export async function GET() {
+async function buildTree(path: string[] = []): Promise<TreeItem[]> {
+  const items: TreeItem[] = await fetchTree(path);
 
-  async function build(path: string[] = []): Promise<TreeItem[]> {
-    const items = await fetchTree(path);
+  const result: TreeItem[] = [];
 
-    const result: TreeItem[] = [];
-
-    for (const item of items) {
-      if (item.type === "dir") {
-        result.push({
-          ...item,
-          children: await build(item.path.split("/")),
-        });
-      } else {
-        result.push(item);
-      }
+  for (const item of items) {
+    if (item.type === "dir") {
+      result.push({
+        ...item,
+        children: await buildTree(item.path.split("/")),
+      });
+    } else {
+      result.push(item);
     }
-
-    return result;
   }
 
-  const tree = await build();
+  return result;
+}
+
+export async function GET() {
+  const tree = await buildTree();
   return NextResponse.json(tree);
 }
